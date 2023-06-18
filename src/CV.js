2fd5b5d203d36c693c8911ac6f34c730293b6c78
joe <joeodams@gmail.com>
2023-06-18 18:05:46 +0100

    Version 1

--- src/CV.js
+++ src/CV.js
@@ -1,90 +1,94 @@
 import "./CV.css";
 import Table from "react-bootstrap/Table";
+import CVRow from "./CVrow";
 
 function CV() {
   return (
     <Table hover className="CV-table">
       <thead></thead>
       <tbody>
-        <tr height="100%" className="CV-table-row">
-          <td style={{ alignItems: "right" }}>
-            <p style={{ fontSize: "18px", fontWeight: "bold" }}>
-              {" "}
-              Software Developer
-            </p>
-            <p>Parmenion</p>
-            <p>June 2022 - </p>
-          </td>
-          <td>
-            <div className="CV-column">
-              <p>
-                Engineer at Parmenion, developing an investment management
-                platform. Worked across a large, mature solution with varying
-                technologies and approaches. Modernisation and infrastructural
-                improvements as well as feature and service development.
-              </p>
-            </div>
-          </td>
-        </tr>
-        <tr className="CV-table-row">
-          <td style={{ alignItems: "right" }}>
-            <p style={{ fontSize: "18px", fontWeight: "bold" }}>
-              {" "}
-              Backend Developer
-            </p>
-            <p>Beedle</p>
-            <p>April 2020 - June 2022</p>
-          </td>
-          <td>
-            <div className="CV-column">
-              <p>
-                Developer for Beedle, a startup developing an MS Teams app.
-                Responsible for rapid implementation of backend functionality
-                according to feature requirements. Working to extend Azure
-                deployments to scale and optimise the app as the customer base
-                grew.{" "}
-              </p>
-            </div>
-          </td>
-        </tr>
+        <CVRow isDivider={true} dividerTitle="Skills"></CVRow>
         <tr>
-          <td style={{ alignItems: "right" }}>
-            <p style={{ fontSize: "18px", fontWeight: "bold" }}>
-              {" "}
-              Developer / Data Scientist
-            </p>
-            <p>Landmark</p>
-            <p>November 2017 - April 2020</p>
-          </td>
-          <td>
-            <div className="CV-column">
-              <p>
-                Data scientist and developer for Landmark, a geographical
-                information services business. Development of data pipelines for
-                the data team. Development of Azure Functions for internal
-                business use
-              </p>
+          {" "}
+          <td></td>
+          <td colSpan={2}>
+            <div
+              className="CV-column"
+              style={{ justifyContent: "center", textAlign: "left" }}
+            >
+              <li>.NET: C#/VB, ASP.NET / Core, Web API</li>
+              <li>API design and development</li>
+              <li>React/TS</li>
+              <li>Archiectural design patterns</li>
+              <li>SOLID principles</li>
+              <li>Code review</li>
+              <li>git</li>
+              <li>Python ML stack: TensorFlow, Keras, scikit-learn, pandas</li>
+              <li>Flask</li>
             </div>
           </td>
-        </tr>{" "}
-        <tr>
-          <td style={{ alignItems: "right" }}>
-            <p style={{ fontSize: "18px", fontWeight: "bold" }}>
-              {" "}
-              Data Analyst
-            </p>
-            <p>Equiniti Data</p>
-            <p>April 2020 - June 2022</p>
-          </td>
-          <td>
-            <div className="CV-column">
-              <p>
-                Worked with on-prem databases to provide marketing data for
-                customers
-              </p>
-            </div>
-          </td>
+          <td></td>
         </tr>
+        <CVRow isDivider={true} dividerTitle="Work"></CVRow>
+        <CVRow
+          jobTitle="Software Developer"
+          companyName="Parmenion"
+          tenure="June 2022 - "
+          jobDescription="Engineer at Parmenion, developing an investment management
+          platform. Worked across a large, mature solution with varying
+          technologies and approaches. Modernisation and infrastructural
+          improvements as well as feature and service development."
+          technologies="ASP.NET Core & Framework using MVC, Web API, Web Forms, EF Core, EF6, React, Docker"
+        ></CVRow>
+        <CVRow
+          jobTitle="Backend Developer"
+          companyName="Beedle Edu"
+          tenure="April 2020 - June 2022"
+          jobDescription="Developer for Beedle, a startup developing an MS Teams app.
+          Responsible for rapid implementation of backend functionality
+          according to feature requirements. Working to extend Azure
+          deployments to scale and optimise the app as the customer base
+          grew."
+          technologies="Modern C# stack, Azure Functions, REST APIs, React/TS, Azure SQL Database"
+        ></CVRow>
+        <CVRow
+          jobTitle="Developer / Data Scientist"
+          companyName="Landmark Information Group"
+          tenure="November 2017 - April 2020"
+          jobDescription="Data scientist and developer for Landmark, a geographical
+          information services business. Development of data pipelines for
+          the data team. Development of Azure Functions for internal
+          business use."
+          technologies="Azure Functions, REST/SOAP APIs, Azure Logic Apps, scikit-learn"
+        ></CVRow>
+        <CVRow
+          jobTitle="Data Analyst"
+          companyName="Equiniti Data"
+          tenure="June 2016 - August 2017"
+          jobDescription="Worked with on-prem databases to provide marketing data for
+          customers."
+          technologies="SQL Server"
+        ></CVRow>
+        <CVRow isDivider={true} dividerTitle="Education"></CVRow>
+        <CVRow
+          jobTitle="University of Sussex"
+          companyName="BSc (Hons.) Physics"
+          tenure="September 2012 - June 2015"
+          jobDescription={[
+            <li>Scientific Computing (1st class)</li>,
+            <li>Mathematical Methods</li>,
+            <li>
+              Final year research project: ‘A Natural Galaxy Classification
+              Scheme’ (68% final grade)
+            </li>,
+          ]}
+        ></CVRow>
+        <CVRow
+          jobTitle="Exeter College"
+          companyName="A-levels"
+          tenure="September 2010 - June 2012"
+          jobDescription={[<li>Maths</li>, <li>Physics</li>, <li>History</li>]}
+        ></CVRow>
       </tbody>
     </Table>
   );
---
+++ src/CVrow.js
@@ -0,0 +1,27 @@
+function CVRow(props) {
+  return (
+    <tr height={props.isDivider ? "100rem" : "200rem"}>
+      <td style={{ alignItems: "right" }}>
+        <p style={{ fontSize: "18px", fontWeight: "bold" }}>
+          {" "}
+          {props.jobTitle}
+        </p>
+        <p>{props.companyName}</p>
+        <p>{props.tenure}</p>
+      </td>
+      <td className="CV-table-text">
+        <div
+          className="CV-column"
+          style={
+            props.isDivider ? { fontWeight: "bold", fontSize: "30px" } : {}
+          }
+        >
+          <p>{props.isDivider ? props.dividerTitle : props.jobDescription} </p>
+        </div>
+      </td>
+      <td className="CV-table-text">{props.technologies}</td>
+    </tr>
+  );
+}
+
+export default CVRow;
